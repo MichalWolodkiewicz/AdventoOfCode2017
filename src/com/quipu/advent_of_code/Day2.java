2279803bcd235fe6d29e8a540eef0e8b30dacc94
Michał Wołodkiewicz <michal.wolodkiewicz@contractors.roche.com>
2017-12-02 11:34:35 +0100

    Day 1.

---
+++ src/com/quipu/advent_of_code/Day2.java
@@ -0,0 +1,27 @@
+package com.quipu.advent_of_code;
+
+class Day2 {
+
+    private final String INPUT =
+            "";
+
+    Day2 runPart1() {
+
+        return this;
+    }
+
+    Day2 runPart2() {
+
+        return this;
+    }
+
+    Day2 runPart1Test() {
+
+        return this;
+    }
+
+    Day2 runPart2Test() {
+
+        return this;
+    }
+}
---
+++ src/com/quipu/advent_of_code/Main.java
@@ -0,0 +1,12 @@
+package com.quipu.advent_of_code;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Day1()
+                .runPart1Test()
+                .runPart1()
+                .runPart2Test()
+                .runPart2();
+    }
+}
